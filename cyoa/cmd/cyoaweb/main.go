ce91c3fbed6e364922812b836448b02f35324400
Pratik Wasekar <pratikwasekar13@gmail.com>
2025-11-20 00:55:54 +0530

    built the http.handler

---
+++ cyoa/cmd/cyoaweb/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"cyoa"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := flag.Int("port", 3000, "the port to start the CYOA web app on")
+	filename := flag.String("file", "gopher.json", "the JSON file with the CYOA story")
+
+	flag.Parse()
+
+	fmt.Printf("Using the story in %s.\n", *filename)
+
+	f, err := os.Open(*filename)
+
+	if err != nil {
+		panic(err)
+	}
+
+	story, err := cyoa.JsonStory(f)
+	if err != nil {
+		panic(err)
+	}
+
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the sever on port: %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
+
+}
---
+++ cyoa/story.go
@@ -0,0 +1,74 @@
+package cyoa
+
+import (
+	"encoding/json"
+	"html/template"
+	"io"
+	"net/http"
+)
+
+var defaultHandlerTmpl = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Choose your own adventure</title>
+</head>
+<body>
+    <h1>{{.Title}}</h1>
+    {{range .Paragraphs}}
+        <p>{{.}}</p>
+    {{end}}
+    <ul>
+    {{range .Options}}
+        <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+    {{end}}
+    </ul> 
+</body>
+</html>`
+
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerTmpl))
+}
+
+var tpl *template.Template
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, h.s["intro"])
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func JsonStory(r io.Reader) (Story, error) {
+	d := json.NewDecoder(r)
+
+	var story Story
+
+	if err := d.Decode(&story); err != nil {
+		return nil, err
+	}
+	return story, nil
+}
+
+type Story map[string]Chapter
+
+type Chapter struct {
+	Title      string   `json:"title"`
+	Paragraphs []string `json:"story"`
+	Options    []Option `json:"options"`
+}
+
+type Option struct {
+	Text    string `json:"text"`
+	Chapter string `json:"arc"`
+}
